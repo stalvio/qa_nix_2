36f0c9214fd221dc9761152e30092174a78e4ae6
stalvio <stalvio.net@gmail.com>
2021-08-27 13:38:00 +0300

    add minor refinements

--- unit_6_next_oop/src/main/java/ua/com/alevel/service/PlanetReporter.java
+++ unit_6_next_oop/src/main/java/ua/com/alevel/service/PlanetReporter.java
@@ -3,6 +3,6 @@ package ua.com.alevel.service;
 public class PlanetReporter {
 
     public static void print(Planet planet) {
-        System.out.println(planet.getClass().getSimpleName() + "'s "+ planet.getAccelerationOfGravity() + "\n");
+        System.out.println(planet.getAccelerationOfGravity() + "\n");
     }
 }
